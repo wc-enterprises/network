34da01d4b26bdf7c80bf28eb41d94fade2ba7af3
Harish A <harish18092002@gmail.com>
2022-11-28 20:41:14 +0530

    network-v1.2.0

---
+++ apps/network-v1.2.0/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'network-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'network-v1.2.0';
+}
